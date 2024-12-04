9f51a35c8e568f510299471cb528a7e128f18874
Kanchan Jadon <kanchan.jadon@snva.com>
2024-12-05 00:25:40 +0530

    add fligth commente

--- controller/searchEngine.controllers.js
+++ controller/searchEngine.controllers.js
@@ -4,7 +4,7 @@ import httpStatus from "http-status";
 export const autoSuggest = async (req , res) => {
     const {cityName , region} = req?.query;
     const result = await findAirportsByCity(cityName);
-    if(!result || result?.length === 0)  return res.status(httpStatus.BAD_REQUEST).json({
+    if(!result || result?.length === 0)  return res.status(httpStatus.NOT_FOUND).json({
       message: "City(Airport) not found"
     });
     
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,6 @@ import searchEngineRoutes from "./searchEngine.routes.js";
 import addFlightRoute from "./addFlight.routes.js";
 
 router.use("/search", searchEngineRoutes);
-router.use("/add-flight", addFlightRoute);
+// router.use("/add-flight", addFlightRoute);
 
 export default router;
